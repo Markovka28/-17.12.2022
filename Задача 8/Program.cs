2f3683d5b3eb674d76716714e6ce015dc719822b
Markovka28 <119870731+Markovka28@users.noreply.github.com>
2022-12-17 22:36:28 +0500

    Update Program.cs

--- Задача 8/Program.cs
+++ Задача 8/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int n = 1;
+            bool not = true;
+
+            Console.WriteLine("Введите число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Чётными числами от 1 до " + a + "являются: ");
+            while (n <= a)
+            {
+                if (n % 2 != 1)
+                {
+                    Console.Write(n + ", ");
+                    not = false;
+                }
+                n++;
+            }
+
+            if (not)
+            {
+                Console.WriteLine("в указанном диапазоне чётных чисел нет");
+            }
+
+
